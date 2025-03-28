6173c10686ea9984c60b22962f0de5fc9d9c7597
Anjan Roy <roy.anjan5@gmail.com>
2025-03-28 22:44:49 +0530

    file2 added

---
+++ file2.py
@@ -0,0 +1,20 @@
+#static method code
+class calculator:
+    @staticmethod
+    def sum(a,b):
+        print(a+b)
+    @staticmethod
+    def sub(a, b):
+        print(a - b)
+
+    @staticmethod
+    def mul(a, b):
+        print(a * b)
+
+    @staticmethod
+    def div(a, b):
+        print(a / b)
+calculator.sum(23,46)
+calculator.sub(45,21)
+calculator.mul(23,89)
+calculator.div(33,3)
