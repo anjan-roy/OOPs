f464ab656612972699d6179147a2638e53246e76
Anjan Roy <roy.anjan5@gmail.com>
2025-04-02 21:35:35 +0530

    file13.py added

---
+++ file13.py
@@ -0,0 +1,14 @@
+'''Inheritance '''
+class Animal:
+    def speak(self):
+        return 'Some sound'
+
+class Dog(Animal):
+    def speak(self):
+        return 'Bark'
+
+dog = Dog()
+print(dog.speak())
+
+
+
