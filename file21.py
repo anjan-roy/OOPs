a5e0bc483b92862d376c3e65dd3138674bd53ad9
Anjan Roy <roy.anjan5@gmail.com>
2025-04-06 23:02:28 +0530

    file21.py added

---
+++ file21.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow"
+
+# Usage
+animals = [Dog(), Cat()]
+for animal in animals:
+    print(animal.make_sound())
