e68d1fa70072c140a723ab2bdf82b00aed169e46
Anjan Roy <roy.anjan5@gmail.com>
2025-04-08 23:20:19 +0530

    file29.py added

---
+++ file29.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+    @abstractmethod
+    def stop_engine(self):
+        pass
+
+class Car(Vehicle):
+    def start_engine(self):
+        print("Car engine started.")
+
+    def stop_engine(self):
+        print("Car engine stopped.")
